61ad777f8915c4da88524c661949c370be80711f
John Reed <reallyjohnreed@gmail.com>
2025-11-18 06:46:44 -0500

    lab-07: working(?) version

--- lab-07/main.c
+++ lab-07/main.c
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  window = SDL_CreateWindow("SDL Example", SDL_WINDOWPOS_UNDEFINED,
+  window = SDL_CreateWindow("Conway's Game of Life", SDL_WINDOWPOS_UNDEFINED,
                             SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
                             SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
   if (!window) {
@@ -55,9 +55,12 @@ int main(int argc, char **argv) {
   SDL_FillRect(screen_surf, NULL, background_color);
   SDL_UpdateWindowSurface(window);
 
+  // char mouse_button_state[2] = {0};
+
   Board gol_board;
+  int initial_cells = 2000;
   int sim_active = 1;
-  init_board(&gol_board, 1000);
+  init_board(&gol_board, initial_cells);
 
   int running = 1;
   SDL_Event event;
@@ -72,8 +75,50 @@ int main(int argc, char **argv) {
       case SDL_KEYDOWN: {
         switch (event.key.keysym.sym) {
 
+        case SDLK_a: {
+          // place 100 new live cells
+          /*
+          for (int rem_attempts = 1000, rem_cells = 100;
+               rem_attempts && rem_cells; rem_attempts--) {
+            rem_cells -= (gol_board.board_flat[rand() % sizeof(Board)] =
+                              CELL_ALIVE) != CELL_DEAD;
+          }
+          */
+
+          unsigned int indexes[sizeof(Board)];
+          for (int i = 0; i < sizeof(Board); i++) {
+            indexes[i] = i;
+          }
+          // shuffle
+          for (int i = sizeof(Board) - 1; i > 0; i--) {
+            int j = rand() % (i + 1);
+            int tmp = indexes[i];
+            indexes[i] = indexes[j];
+            indexes[j] = tmp;
+          }
+
+          for (int i = 0, rem_cells = 100; rem_cells > 0 && i < sizeof(Board);
+               i++) {
+            int idx = indexes[i];
+            if (gol_board.board_flat[idx] != CELL_DEAD)
+              continue;
+
+            gol_board.board_flat[idx] = CELL_ALIVE;
+            rem_cells -= 1;
+          }
+        } break;
+
+        case SDLK_p:
+          sim_active = !sim_active;
+          break;
+
         case SDLK_r:
-          // SDL_FillRect(screen_surf, NULL, rand_color(screen_surf->format));
+          init_board(&gol_board, initial_cells);
+          break;
+
+        case SDLK_s:
+          if (!sim_active)
+            next_generation(&gol_board);
           break;
 
         case SDLK_q:
@@ -94,7 +139,31 @@ int main(int argc, char **argv) {
           SDL_FillRect(screen_surf, NULL, background_color);
           break;
         }
-      }
+      } break;
+
+      case SDL_MOUSEBUTTONDOWN: {
+        if (event.button.x > BOARD_WIDTH * 5 ||
+            event.button.y > BOARD_HEIGHT * 5)
+          break;
+
+        if (event.button.button == SDL_BUTTON_LEFT) {
+          gol_board.board[event.button.y / 5][event.button.x / 5] = CELL_ALIVE;
+        } else if (event.motion.state == SDL_BUTTON_RIGHT) {
+          gol_board.board[event.button.y / 5][event.button.x / 5] = CELL_DEAD;
+        }
+      } break;
+
+      case SDL_MOUSEMOTION: {
+        if (event.motion.x > BOARD_WIDTH * 5 ||
+            event.motion.y > BOARD_HEIGHT * 5)
+          break;
+
+        if (event.motion.state & SDL_BUTTON_LMASK) {
+          gol_board.board[event.motion.y / 5][event.motion.x / 5] = CELL_ALIVE;
+        } else if (event.motion.state & SDL_BUTTON_RMASK) {
+          gol_board.board[event.motion.y / 5][event.motion.x / 5] = CELL_DEAD;
+        }
+      } break;
 
       default:
         break;
